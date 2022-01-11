c6ec70bbbfcc8c4b36549e89fa216b59c1872898
Vinicius Leite <viniciusleite094@gmail.com>
2022-01-11 20:07:01 -0300

    Automapper

---
+++ LojaWeb/Automapper/AutomapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LojaWeb.Models;
+using LojaWeb.Models.ModeloDados;
+
+namespace LojaWeb.Automapper
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<ClienteModel, ClienteViewModel>().ReverseMap();
+        }
+    }
+}
