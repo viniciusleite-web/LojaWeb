f1378dcf2bd24b89ed5d9f95adf748a2accb9286
Vinicius Leite <viniciusleite094@gmail.com>
2022-01-11 14:03:58 -0300

    Project created

---
+++ LojaWeb/Models/ClienteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaWeb.Models
+{
+    public class ClienteViewModel
+    {
+    }
+}
